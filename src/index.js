a0afdd9c4f6ac169d0f4272b570fd24fc3203d23
DillonB07 <dillonbarnes07@gmail.com>
2022-02-07 18:46:07 +0000

    Add basic textbox and button along with simple CSS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class ToxBox extends React.Component {
+	render() {
+		return (
+			<div className="toxbox">
+				<textarea className="tox-input" rows={10}></textarea>
+				<button className="tox-button">Send</button>
+			</div>
+		);
+	}
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Page extends React.Component {
+	render() {
+		return <ToxBox />;
+	}
+}
+
+// ========================================
+
+ReactDOM.render(<Page />, document.getElementById('root'));
